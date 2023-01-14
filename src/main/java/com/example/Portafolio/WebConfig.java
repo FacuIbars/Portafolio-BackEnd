9acb643b4fc15b6cd7167b585eb48054d6270674
FacuIbars <facuibars@gmail.com>
2023-01-14 03:13:33 -0300

    deploy koyeb

--- src/main/java/com/example/Portafolio/WebConfig.java
+++ src/main/java/com/example/Portafolio/WebConfig.java
@@ -5,14 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("auth/login").allowedOrigins("http://localhost:4200/","https://portafolio-adf0a.web.app/")
-                .allowedMethods("*").exposedHeaders("*");
-
+        registry.addMapping("/**");
     }
 }
